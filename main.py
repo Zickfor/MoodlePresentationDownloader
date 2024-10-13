c88a69b77d6a92639f56b589a5502c5d54a19b38
Dima <megahouse243@gmail.com>
2024-10-13 20:38:13 +0300

    Fixed bug with temp_folder naming

--- main.py
+++ main.py
@@ -25,28 +25,28 @@ def img_converter_to_pdf(dirname):
         f.write(img2pdf.convert(imgs))
 
 
-def download_presentation(url):
-    driver.get(url)
-    WebDriverWait(driver, 5)
+def download_presentation(pres_id, moodle_url):
+    driver.get(moodle_url + "mod/page/view.php?id=" + pres_id)
+    WebDriverWait(driver, 10)
     title = driver.find_element(By.TAG_NAME, "h1")
     new_title = re.sub(r'[^\w]', ' ', title.text)
-    if not os.path.exists(new_title):
-        os.mkdir(new_title)
+    if not os.path.exists(pres_id):
+        os.mkdir(pres_id)
     while True:
         page = driver.find_element(By.CLASS_NAME, "info")
         current_page, last_page = re.split(r" / ", page.text)
         print(f"{current_page} / {last_page}")
         viewport = driver.find_element(By.CLASS_NAME, "viewport")
         base64_img = viewport.get_property("style")["background-image"][27:-2]
-        with open(f"{new_title}/{current_page.zfill(3)}.jpg", "wb") as file:
+        with open(f"{pres_id}/{current_page.zfill(3)}.jpg", "wb") as file:
             file.write(base64.b64decode(base64_img))
             file.close()
         if current_page == last_page:
             break
         next_button = driver.find_elements(By.TAG_NAME, "svg")
         next_button[1].click()
-    img_converter_to_pdf(new_title)
-    shutil.rmtree(new_title)
+    img_converter_to_pdf(pres_id)
+    shutil.rmtree(pres_id)
 
 
 if __name__ == "__main__":
@@ -58,10 +58,10 @@ if __name__ == "__main__":
                 "password": "Your password here...",
                 "moodle_url": "https://example.com/"
             },
-            "urls": [
-                "https://example.com/mod/page/view.php?id=109554",
-                "https://example.com/mod/page/view.php?id=109569",
-                "https://example.com/mod/page/view.php?id=147600"
+            "ids": [
+                "109554",
+                "109569",
+                "147600"
             ]
         }
         with open("config.toml", "w") as f:
@@ -83,10 +83,10 @@ if __name__ == "__main__":
     login = driver.find_element(By.ID, "loginbtn")
     login.click()
 
-    WebDriverWait(driver, 5)
+    WebDriverWait(driver, 10)
 
-    for pres in config["urls"]:
+    for pres in config["ids"]:
         print(pres)
-        download_presentation(pres)
+        download_presentation(pres, config["auth"]["moodle_url"])
 
     driver.quit()
